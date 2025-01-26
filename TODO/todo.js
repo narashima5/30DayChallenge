e676fca07f7334c5ce17adf06355bbef6f769da8
narashima5 <141750089+narashima5@users.noreply.github.com>
2025-01-26 14:11:09 +0530

    Add files via upload

---
+++ TODO/todo.js
@@ -0,0 +1,49 @@
+var nodeList = document.getElementsByTagName('LI');
+for (let i = 0; i < nodeList.length; i++) {
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u00D7');
+    span.className = 'close';
+    nodeList[i].appendChild(span);
+    
+}
+
+var close = document.getElementsByClassName('close');
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = 'none'
+    }   
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click',function(ev){
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+},false)
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("input").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
