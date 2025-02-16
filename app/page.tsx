901704e165e1796abb301f0ba1952372a5e242eb
narashima5 <141750089+narashima5@users.noreply.github.com>
2025-02-16 16:28:48 +0530

    Api call

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,96 @@
 'use client'
-import Image from "next/image";
-import img1 from './img1.jpg';
-import img2 from './img2.jpg';
-import img3 from './img3.jpg';
-import img4 from './img4.jpg';
-import img5 from './img5.jpeg';
 import styles from "./page.module.css";
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
+
+interface Todo {
+  id? : number,
+  userId? : number,
+  title? : String,
+  completed? : Boolean
+}
 
 export default function Home() {
-
-  const [topText,setTopText] = useState('');
-  const [bottomText,setBottomText] = useState('');
-
-  const images = [img1,img2,img3,img4,img5];
-  const [imgSelect,setImgSelect] = useState(images[0]);
-
-  const handleGenerateImg = () => {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    setImgSelect(images[randomIndex]);
-  }
-
-  const handleTopInput = (event: { target: { value: SetStateAction<string>; }; }) => {
-    setTopText(event.target.value);
-  }
-  const handleBottomInput = (event: { target: { value: SetStateAction<string>; }; }) => {
-    setBottomText(event.target.value);
+  const [users, setUsers] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [hdData,setHdData] = useState<String[]>([]);
+  const [filter,setFilter] = useState<String | number>("");
+  const [filterData,setFilterData] = useState<String | number>('');
+  const [url,setUrl] = useState('');
+  
+  let i = 0;
+  
+  const handleClick = () => {
+    setLoading(true);
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        setUsers(json)
+        setHdData(Object.keys(json[0]))
+      })
+      .catch((e) => {
+        alert("Error: Not Valid Json");
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   return (
     <div className={styles.main}>
-      <h2>MEME GENERATOR</h2>
-      <div className={styles.inputs}>
-        <label htmlFor="top">Enter top text</label>
-        <input type="text" name="top" className={styles.input} onChange={handleTopInput}/>
-        <label htmlFor="bottom">Enter bottom text</label>
-        <input type="text" name="bottom" className={styles.input} onChange={handleBottomInput}/>
-        <button className={styles.btn} onClick={handleGenerateImg}>Generate new image</button>
-      </div>
+      <input type="text" className={styles.urlinput} onChange={(e) => setUrl(e.target.value)}/>
+      <button className={styles.btn} onClick={handleClick}>
+        CLICK TO MAKE API CALL
+      </button>
 
-      <div className={styles.memeContainer}>
-        <Image src={imgSelect} alt="" className={styles.img}></Image>
-        <h1 className={styles.top}>{topText}</h1>
-        <h1 className={styles.bottom}>{bottomText}</h1>
+      <div className={styles.selectContainer}>
+        <select name="filters" id="fill" className={styles.selection} onChange={(e) => setFilter(e.target.value)}>
+          <option value="none" selected disabled hidden={filter?true:false}>SELECT AN OPTION</option>
+          {hdData.map((value) => <option key={++i} value={value.toString()}>{value.toUpperCase()}</option>)}
+        </select>
+
+        <input type="text" className={styles.fill_input} disabled={filter?false:true} placeholder={filter ? `SEARCH FOR ${filter.toString().toUpperCase()}` : "SEARCH"} onChange={(e) => setFilterData(e.target.value)}/>
       </div>
+      
+      {loading ? (
+        <div className={styles.loader}></div>
+      ) : (
+        <>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                {
+                  hdData.map((value) => <th key={++i}>{value.toUpperCase()}</th>)
+                }
+              </tr>
+            </thead>
+            <tbody>
+              {
+                filter === '' || filterData === '' ? (
+                  users.map(user => (
+                    <tr key={user.id}>
+                      <td>{user.userId}</td>
+                      <td>{user.id}</td>
+                      <td>{user.title?.toUpperCase()}</td>
+                      <td>{user.completed?.toString().toUpperCase() ?? 'N/A'}</td>
+                    </tr>
+                  ))
+                ) : (
+                  users.filter(user => 
+                    (user as any)[filter as keyof typeof user]?.toString() === filterData.toString().toLocaleLowerCase()
+                  ).map(user => (
+                    <tr key={user.id}>
+                      <td>{user.userId}</td>
+                      <td>{user.id}</td>
+                      <td>{user.title?.toUpperCase()}</td>
+                      <td>{user.completed?.toString().toUpperCase() ?? 'N/A'}</td>
+                    </tr>
+                  ))
+                )
+              }
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   );
 }
